d0283c6e227c3db118a619565b785258b5f4b59b
ⁿᵏGwee⚝ <160815798+31533317@users.noreply.github.com>
2026-02-20 18:11:07 -0400

    Update fb.js

--- commands/dow/fb.js
+++ commands/dow/fb.js
@@ -12,7 +12,7 @@ export default {
     }
 
     try {
-      const videoUrl = `https://api.evogb.org/dl/facebook2?url=${args[0]}&key=api-E7tjU`
+      const videoUrl = `https://api.evogb.org/dl/facebook2?url=${args[0]}&key=evogb-UAXrKG3d`
 
       const response = await fetch(videoUrl)
       if (!response.ok) throw new Error(`HTTP ${response.status}`)
